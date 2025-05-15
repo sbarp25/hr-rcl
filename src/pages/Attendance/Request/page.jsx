de485ac00e03aaa2b5f381ecc4104b3831980d2c
odinson-codes <lamichhaneumang123@gmail.com>
2025-05-15 18:36:38 +0545

    fix: Update API endpoint for fetching late check-in data to use role-based reviews

--- src/pages/Attendance/Request/page.jsx
+++ src/pages/Attendance/Request/page.jsx
@@ -238,7 +238,7 @@ const AttendanceRequest = () => {
     setIsLoading(true);
     try {
       const response = await axiosInstance.post(
-        "/api/v1/attendance/late-check-in/my-late-check-ins",
+        "/api/v1/attendance/late-check-in/role-based-reviews",
         {
           // const response = await axiosInstance.post("/api/v1/auth/get/all", {
           pageIndex: currentPage,
